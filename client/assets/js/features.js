e04714f4ba067e54bb73fd7d42c618e63bae0a85
Fadhil Muhammad <bringitdown0@gmail.com>
2020-04-03 02:14:30 +0700

    little things

--- client/assets/js/features.js
+++ client/assets/js/features.js
@@ -5,4 +5,32 @@ $(document).ready(function () {
     $('.dropdown').toggleClass('is-active');
   });
   // ==========
+
+  // Routes from @JulioSabandar
+  // ==========
+  // routerMusic.get('/music', controllerMusic.getLikedSongs);
+  // routerMusic.get('/music/search', controllerMusic.getSongBySearch);
+  // routerMusic.post('/music/:id', controllerMusic.addToLikedSongs);
+  // routerMusic.delete('/music/:id', controllerMusic.deleteLikedSongs);
+
+  // Music search
+  const url = 'http://localhost:3000/music/search';
+
+  $('#music-search').submit((event) => {
+    console.log('search is clicked');
+    event.preventDefault();
+
+    $.ajax({
+      type: 'GET',
+      url,
+      data,
+      dataType: 'json',
+    })
+      .done((result) => {
+        console.log(result);
+      })
+      .fail((err) => {
+        console.log(err);
+      });
+  });
 });
