6b157d388963493dfd8ce9caf9965ddb6193164c
Fadhil Muhammad <bringitdown0@gmail.com>
2020-04-03 10:19:47 +0700

    bang maleek cek dulu

--- client/assets/js/features.js
+++ client/assets/js/features.js
@@ -13,24 +13,92 @@ $(document).ready(function () {
   // routerMusic.post('/music/:id', controllerMusic.addToLikedSongs);
   // routerMusic.delete('/music/:id', controllerMusic.deleteLikedSongs);
 
-  // Music search
-  const url = 'http://localhost:3000/music/search';
+  // Auths from @2maleek and @fuza-ail
+  // ==========
 
-  $('#music-search').submit((event) => {
-    console.log('search is clicked');
-    event.preventDefault();
+  // defaults
+  $('#logout').hide();
 
-    $.ajax({
-      type: 'GET',
-      url,
-      data,
-      dataType: 'json',
-    })
-      .done((result) => {
-        console.log(result);
-      })
-      .fail((err) => {
-        console.log(err);
-      });
+  // register
+  $('#register').on('submit', (e) => {
+    e.preventDefault();
+    register();
+  });
+
+  // login
+  $('#login').on('submit', (e) => {
+    e.preventDefault();
+    login();
+    $('#sign-in-top').hide();
+    $('#logout').show();
+  });
+
+  // logout
+  $('#logout').on('click', (e) => {
+    e.preventDefault();
+    logout();
   });
 });
+
+// register funtion - @2maleek
+const register = () => {
+  let data = {
+    email: $('#emailRegister').val(),
+    password: $('#passwordRegister').val(),
+  };
+  $.ajax({
+    type: 'POST',
+    url: 'http://localhost:3000/register',
+    data: data,
+    dataType: 'json',
+  })
+    .done(function (token) {
+      console.log(token.access_token);
+      localStorage.setItem('access_token', token.access_token);
+
+      // remove drops
+      $('.dropdown').toggleClass('is-active');
+
+      // swal
+      Swal.fire('Thank you!', 'Register success', 'success');
+    })
+    .fail(function (err) {
+      console.log(err.responseJSON.errors[0].message);
+    });
+};
+
+// login - @2maleek
+const login = () => {
+  let data = {
+    email: $('#emailLogin').val(),
+    password: $('#passwordLogin').val(),
+  };
+  $.ajax({
+    type: 'POST',
+    url: 'http://localhost:3000/login',
+    data: data,
+    dataType: 'json',
+  })
+    .done(function (token) {
+      console.log(token.access_token);
+      localStorage.setItem('access_token', token.access_token);
+
+      // remove drops
+    })
+    .fail(function (err) {
+      if (err.status === 500) {
+        console.log('Internal server error!');
+      } else {
+        console.log(err.responseJSON.error);
+      }
+    });
+};
+
+// logout - @2maleek
+const logout = () => {
+  localStorage.removeItem('access_token');
+  var auth2 = gapi.auth2.getAuthInstance();
+  auth2.signOut().then(function () {
+    console.log('User signed out.');
+  });
+};
--- server/app.js
+++ server/app.js
@@ -1,16 +1,18 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 const cors = require('cors');
 
-const routerUser = require('./routes')
+const routerUser = require('./routes');
 
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
 app.use(routerUser);
 
-app.listen(PORT,()=>{
-  console.log(`Listengin to port ${PORT}`)
+app.listen(PORT, () => {
+  console.log(`Listengin to port ${PORT}`);
 });
+
